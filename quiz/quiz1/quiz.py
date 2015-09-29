033ba37fb84b965bb4ac3c9c56385b1c25581c13
Ahmad <ahmad.melegy@gmail.com>
2015-09-29 03:26:37 +0200

    Add quiz1 solution

--- quiz/quiz1/quiz.py
+++ quiz/quiz1/quiz.py
@@ -9,10 +9,10 @@ old_new_salaries = [
 ]
 
 def is_high_raise(r):
-    ???
+    return r > 500
 
-raises = map(???, old_new_salaries)
+raises = map(lambda x: x[1] - x[0], old_new_salaries)
 high_raises = filter(is_high_raise, raises)
-total_high_raises = reduce(lambda a,b: a + b, high_raises)
+total_high_raises = reduce(lambda x,y: x + y, high_raises)
 
 print "total high raises: %s" % total_high_raises
