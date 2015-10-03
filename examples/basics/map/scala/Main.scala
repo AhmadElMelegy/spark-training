fca75ca2c4443bdbeb28ad5570f0b2261ff40f33
Ahmad <ahmad.melegy@gmail.com>
2015-10-03 15:42:00 +0200

    Solve Map example

--- examples/basics/map/scala/Main.scala
+++ examples/basics/map/scala/Main.scala
@@ -4,7 +4,9 @@ object Main {
   def main(args: Array[String]) {
     val conf = new SparkConf().setAppName("test").setMaster("local[*]")
     val sc = new SparkContext(conf)
-
+    val words= sc.parallelize(Seq("hello","world"))
+    val uperredWords = words.map(_.toUpperCase).collect()
+    uperredWords.foreach(println)
   
   }
 }
