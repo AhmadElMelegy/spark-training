1fec2792d5cdab4138cc1dad5eb61664119d4212
Ahmad <ahmad.melegy@gmail.com>
2015-10-03 15:40:43 +0200

    Solve intersection example

--- examples/basics/intersection/scala/Main.scala
+++ examples/basics/intersection/scala/Main.scala
@@ -5,6 +5,8 @@ object Main {
     val conf = new SparkConf().setAppName("test").setMaster("local[*]")
     val sc = new SparkContext(conf)
 
-  
+    val values1 = sc.parallelize(Seq("ahmad","amr","tamer","ali"))
+    val values2 = sc.parallelize(Seq("ali","tamer","alaa"))
+    values1.intersection(values2).collect
   }
 }
