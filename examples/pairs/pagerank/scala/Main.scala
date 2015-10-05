18b97041a1a13e054bc77ab88f4ea1c0ff075dc0
Ahmad <ahmad.melegy@gmail.com>
2015-10-05 15:52:25 +0200

    Solve pageRank example

--- examples/pairs/pagerank/scala/Main.scala
+++ examples/pairs/pagerank/scala/Main.scala
@@ -5,6 +5,20 @@ object Main {
     val conf = new SparkConf().setAppName("test").setMaster("local[*]")
     val sc = new SparkContext(conf)
 
-  
+    val connections = sc.textFile("../input").map(_.split(": ")).map(o=>o{0}->o{1}.split(", "))
+    connections.foreach(o=>println(o._1 + " :  " + o._2.mkString(" ") ))
+
+    var ranks = connections.mapValues(v => 1.0)
+    ranks.foreach(println(_))
+
+    for(i <- 1 to 100 ) {
+      val contrib = connections.join(ranks).flatMap {
+        case (node,(links,rank)) =>
+          links.map(link => link -> rank / links.length)
+      }
+      ranks = contrib.reduceByKey(_+_).mapValues(v => .15 + .85 * v)
+    }
+    ranks.foreach(println(_))
+    ranks.saveAsTextFile("ranks")
   }
 }
