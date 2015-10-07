3bad8e939a52a80aa3bb26751325ab0a4aa58f6e
Ahmad <ahmad.melegy@gmail.com>
2015-10-07 15:54:32 +0200

    Solve wordCount example

--- examples/pairs/wordcount/scala/Main.scala
+++ examples/pairs/wordcount/scala/Main.scala
@@ -4,7 +4,9 @@ object Main {
   def main(args: Array[String]) {
     val conf = new SparkConf().setAppName("test").setMaster("local[*]")
     val sc = new SparkContext(conf)
-
+    val pairs = sc.textFile("../input").flatMap(_.split("\\W+")).map(_->1).reduceByKey(_+_)
+    pairs.saveAsTextFile("result")
+    pairs.foreach(o=>println(o._1 +  " ->" + o._2))
 
   }
 }
